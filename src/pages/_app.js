913d437243f0886712412c76f398ee1f95e04c55
Mehrshad Moradshan <mehr.m6534@gmail.com>
2023-01-30 19:34:17 +0330

    fix index file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,8 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
+import '../../public/css/boxicons.min.css';
+import '../../public/css/styles.css';
+// import '../../public/fonts/boxicons.eot';
+// import '../../public/css/styles.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
