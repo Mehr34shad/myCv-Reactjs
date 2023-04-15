255ae2f023bfccb90a192df22f3c6424dcb1ed7c
Mehrshad Moradshan <mehr.m6534@gmail.com>
2023-04-15 12:00:04 +0330

    hfhf

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import "../../public/css/boxicons.min.css";
-// import "../../public/css/aos.min.css";
-import "../../public/css/styles.css";
+import "../public/css/boxicons.min.css";
+import "../public/css/styles.css";
 
 export default function App({ Component, pageProps }) {
   return (
